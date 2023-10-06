770db1873c28f41ca5dc1e866d2d5c148f9c286a
Sahana J P <sjp@synamedia.com>
2023-10-06 22:45:00 +0530

    adding components

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import styles from "./welcome.style";
+import { useRouter } from "expo-router";
+
+const Welcome = () => {
+  const router = useRouter();
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello sjp</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
